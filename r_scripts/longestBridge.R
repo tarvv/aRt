b481b5cbf2a521c46584c991b4758aa616cf4866
tarvv <47506368+tarvv@users.noreply.github.com>
2023-01-16 16:14:15 -0600

    Reworked and added comments

--- r_scripts/longestBridge.R
+++ r_scripts/longestBridge.R
@@ -1,79 +1,61 @@
-library(sf)
 library(tidyverse)
-library(ipumsr)
 library(tidycensus)
+library(sf)
+library(ipumsr)
 library(wesanderson)
 library(gridExtra)
-
-# ACS5 2019 DATA
+options(tigris_use_cache = TRUE)
 # Census key
-# census_api_key("YOUR API KEY GOES HERE")
 readRenviron("~/.Renviron")
 
-#Search for data
-# query <- load_variables(2019, "acs5", cache = TRUE)
-# query %>%
-#   mutate(table = str_extract(name, "^.+_")) %>%
-#   filter(str_detect(concept, "RACE")) %>%
-#   select(table, concept) %>% distinct %>% print(n = Inf)
+#function for generating samples
+genSamples <- function(data)
+  st_sample(data, size = round(data$value / 10))
 
-#get 5 year census data 2019
-acs19 <- get_acs("tract", table = "B02001", cache_table = TRUE, geometry = TRUE,
-               state = "55", county = "079", output = "tidy")
+# get 2020 decennial census race data from census API
+data2020 <- get_decennial("tract", table = "P1", cache_table = TRUE, year = 2020,
+                      geometry = TRUE, state = "55", county = "079",
+                      output = "tidy")
 
-acs19 <- acs19 %>%
-  mutate(id = str_extract(variable, "[0-9]{3}$") %>% as.integer) %>%
-  filter(id == 2 | id == 3) %>%
-  mutate(race = case_when(
-    # id == 1 ~ "Sum All",
-    id == 2 ~ "White",
-    id == 3 ~ "Black",
-    # id == 4 ~ "Native American",
-    # id == 5 ~ "Asian",
-    # id == 6 ~ "Pacific Islander",
-    # id == 7 ~ "Some Other",
-    # id > 7 ~ "Two or More"
+# get 1960 decennial census race data stored locally
+# source: Steven Manson, Jonathan Schroeder, David Van Riper, Tracy Kugler, and Steven Ruggles. IPUMS National Historical Geographic Information System: Version 17.0 [dataset]. Minneapolis, MN: IPUMS. 2022. http://doi.org/10.18128/D050.V17.0
+shp_zip_loc60 <- "data/nhgis0001_shape.zip"
+dat_loc60 <-  "data/nhgis0001_ds92_1960_tract.csv"
+
+data2020 <- data2020 %>%
+  filter(variable == "P1_003N" | variable == "P1_004N") %>%   # filter so only rows with Black and White counts remain
+  mutate(race = case_when(          # Change variable names for clarity 
+    variable == "P1_003N" ~ "White",
+    variable == "P1_004N" ~ "Black",
   )) %>%
-  group_by(GEOID, race) %>%
-  summarize(estimate = sum(estimate)) %>%
-  filter(estimate > 50) %>%
-  split(.$race)
+  group_by(GEOID, race) %>%         # group data to work with summarize()
+   summarize(value = value) #%>%   
+   filter(value > 50) %>%            # filter to census tracts with > 50 people
+   split(.$race)       # converts dataframe into a list of dataframes, 1 for each race
 
-generate_samples <- function(data)
-  suppressMessages(st_sample(data, size = round(data$estimate / 10)))
+pts2020 <- map(data2020, genSamples) %>%
+  imap(~st_sf(tibble(race = rep(.y, length(.x))), geometry = .x))
+pts2020 <- do.call(rbind, pts2020)
 
-pts19 <- map(acs19, generate_samples) %>%
-  imap(~st_sf(data_frame(race = rep(.y, length(.x))), geometry = .x))
-
-pts19 <- do.call(rbind, pts19)
-
-pts19 <- pts19 %>% group_by(race) %>%
-  summarise() %>%
+pts2020 <- pts2020 %>%
+  group_by(race) %>%
+  summarize() %>%
   mutate(race = factor(race, levels = c("White", "Black")))
 
-
-# DECCENIAL 1960'S DATA
-shp_zip_loc60 <- paste0(getwd(), "/misc/nhgis0001_shape.zip")
-dat_loc60 <- paste0(getwd(), "/data/nhgis0001_ds92_1960_tract.csv")
-
-dec60 <- read_nhgis_sf(dat_loc60, shp_zip_loc60, verbose = FALSE) %>%
+data1960 <- read_nhgis_sf(dat_loc60, shp_zip_loc60, verbose = FALSE) %>%
   st_transform(4269) %>%
   filter(STATEA == "55" & COUNTYA == "079") %>%
-  select(GISJOIN2, geometry, B7B001, B7B002) %>%
-  rename("GEOID" = GISJOIN2, "White" = B7B001, "Black" = B7B002) %>%
-  gather("race", "estimate", -GEOID, -geometry) %>%
+  select(GEOID = GISJOIN2, geometry, White = B7B001, Black = B7B002) %>%
+  gather("race", "value", -GEOID, -geometry) %>%
   group_by(GEOID, race) %>%
-  summarize(estimate = sum(estimate)) %>%
-  filter(estimate > 50) %>%
+  summarize(value = sum(value)) %>%
+  filter(value > 50) %>%
   split(.$race)
 
-generate_samples60 <- function(data)
-  suppressMessages(st_sample(data, size = round(data$estimate / 10)))
-
-pts60 <- map(dec60, generate_samples60) %>%
+pts1960 <- map(data1960, genSamples) %>%
   imap(~st_sf(data_frame(race = rep(.y, length(.x))), geometry = .x))
-pts60 <- do.call(rbind, pts60)
-pts60 <- pts60 %>% group_by(race) %>% summarise() %>%
+pts1960 <- do.call(rbind, pts1960)
+pts1960 <- pts1960 %>% group_by(race) %>% summarise() %>%
   mutate(race = factor(race, levels = c("White", "Black")))
 
 # setting theme options
@@ -87,9 +69,9 @@ theme_set(theme_minimal() +
                   axis.text = element_blank(),
                   legend.position = "none"))
 
-#PLOT ACS5 2019
-p19 <- ggplot() +
-  geom_sf(data = pts19, aes(colour = race, fill = race), size = .5, stroke = 0) +
+#PLOT DEC 2020
+plot20 <- ggplot() +
+  geom_sf(data = pts2020, aes(colour = race, fill = race), size = .75, stroke = 0) +
   scale_color_manual(values = wes_palette(n=2, name="Royal1")) +
   scale_alpha(range = c(.1,1)) +
   #bridge east line
@@ -101,15 +83,11 @@ p19 <- ggplot() +
   annotate("path",
            x = c(-87.934729, -87.933929, -87.934046, -87.934846),
            y = c(43.037156, 43.036156, 43.024232, 43.023232),
-           size = 1, color = "black", lineend = "round") #+
-  #Freedom house
-  # annotate("point", y=43.049870142373045, x=-87.93149064420359, color="black") +
-  # coord_sf(xlim = c(-88, -87.82713), ylim = c(42.96, 43.1))
+           size = 1, color = "black", lineend = "round")
 
-
-#PLOT DEC 1960'S
-p60 <- ggplot() +
-  geom_sf(data = pts60, aes(colour = race, fill = race), size = .5, stroke = 0) +
+#PLOT DEC 1960
+plot60 <- ggplot() +
+  geom_sf(data = pts1960, aes(colour = race, fill = race), size = .5, stroke = 0) +
   scale_color_manual(values = wes_palette(n=2, name="Royal1")) +
   #bridge east line
   annotate("path",
@@ -120,11 +98,8 @@ p60 <- ggplot() +
   annotate("path",
            x = c(-87.934729, -87.933929, -87.934046, -87.934846),
            y = c(43.037156, 43.036156, 43.024232, 43.023232),
-           size = 1, color = "black", lineend = "round") #+
-  #Freedom house
-  # annotate("point", y=43.049870142373045, x=-87.93149064420359, color="black") +
-  # coord_sf(xlim = c(-88, -87.82713), ylim = c(42.96, 43.1))
+           size = 1, color = "black", lineend = "round")
 
-pmain <- grid.arrange(p60, p19, nrow = 1)
+censusPlot <- grid.arrange(plot60, plot20, nrow = 1)
 
-ggsave("fin/longestBridge.png", plot = pmain, width = 14, height = 14)
+ggsave("longestBridge.png", plot = censusPlot, width = 12, height = 14)
